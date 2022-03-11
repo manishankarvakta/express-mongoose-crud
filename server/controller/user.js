8456b6bb7fd0e57336fba62e819c56f5b8c499a9
Manishankar Vakta <manishankarvakta@gmail.com>
2022-03-12 02:38:05 +0600

    CRUD API creation done

--- server/controller/user.js
+++ server/controller/user.js
@@ -31,17 +31,60 @@ exports.create = (req, res)=>{
 
 // get users
 exports.find = (req, res)=>{
-
+    User.find()
+    .then(user=>{
+        res.send(user)
+    })
+    .catch(err=>{
+        res.status(500).send({message: err.message || 'Problem finding user'})
+    })
 }
 
 
 // update user 
 exports.update = (req, res)=>{
+    // validate request
+    if(!req.body){
+        res.status(400)
+        .send({message: 'content can not be empty!'});
+        return;
+    }
 
+    const id = req.params.id;
+      
+    User
+    .findOneAndUpdate(id, req.body, {new: true})
+    .then(data=>{
+        if(!data){
+            res.status(500)
+            .send({message: `Can't update user with id ${id}. Maybe user not found.`})
+        }else{
+            res.send(data)
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message: `Error update user informations.`})
+
+    })
 }
 
 
 // delete user 
 exports.delete = (req, res)=>{
+    const id = req.params.id;
+      
+    User
+    .findOneAndDelete(id)
+    .then(data=>{
+        if(!data){
+            res.status(500)
+            .send({message: `Can't Delete user with id ${id}. Maybe user not found.`})
+        }else{
+            res.send({message: "User was delete successfully"})
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message: `Error Delete user with id ${id}.`})
 
+    })
 }
